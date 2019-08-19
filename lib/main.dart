c37cb7462c5259622dd7edd24e03185448d6bd5f
chiapin <chiapin@hellotalk.com>
2019-08-19 19:48:43 +0800

    init tabcontroler

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'model/post.dart';
 
 // void main() {
 //   return runApp(App());
@@ -11,6 +10,7 @@ class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Home(),
       theme: ThemeData(
         primarySwatch: Colors.yellow
@@ -20,57 +20,48 @@ class App extends StatelessWidget {
 }
 
 class Home extends StatelessWidget {
-  Widget _listItemBuilder(BuildContext context, int index) {
-    return Container(
-      color: Colors.white,
-      margin: EdgeInsets.all(8.0),
-      child: Column(
-        children: <Widget>[
-          Image.network(posts[index].imageUrl),
-          SizedBox(height: 16.0),
-          Text(
-            posts[index].title,
-            style: Theme.of(context).textTheme.title,
-          ),
-          Text(
-            posts[index].author,
-            style: Theme.of(context).textTheme.subhead,
-          ),
-          SizedBox(height: 16.0)
-        ],
-      ),
-    );
-  }
-
   @override
     Widget build(BuildContext context){
-      return Scaffold(
+      return DefaultTabController(
+        length: 3,
+        child: Scaffold(
           backgroundColor: Colors.grey[100],
           appBar: AppBar(
+            leading: IconButton(
+              icon: Icon(Icons.menu),
+              tooltip: 'Navigration',
+              onPressed: () => debugPrint('Navigration button is pressed.'),
+            ),
             title: Text('nihao'),
+            actions: <Widget>[
+              IconButton(
+                icon: Icon(Icons.search),
+                tooltip: 'Search',
+                onPressed: () => debugPrint('Search button is pressed.'),
+              ),
+              // IconButton(
+              //   icon: Icon(Icons.more_horiz),
+              //   tooltip: 'Search',
+              //   onPressed: () => debugPrint('Search button is pressed.'),
+              // )
+            ],
             elevation: 0.0,
+            bottom: TabBar(
+              tabs: <Widget>[
+                Tab(icon: Icon(Icons.local_florist),),
+                Tab(icon: Icon(Icons.change_history),),
+                Tab(icon: Icon(Icons.directions_bike),),
+              ],
+            ),
           ),
-          body: ListView.builder(
-            itemCount: posts.length,
-            itemBuilder: _listItemBuilder,
+          body: TabBarView(
+            children: <Widget>[
+              Icon(Icons.local_florist, size:128.0, color: Colors.black12,),
+              Icon(Icons.change_history, size: 128.0, color: Colors.black12,),
+              Icon(Icons.directions_bike, size: 128.0, color: Colors.black12,),
+            ],
           )
-        );
+        ),
+      );
     }
 }
-
-class Hello extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Center(
-      child: Text(
-        'hello world',
-        textDirection: TextDirection.ltr,
-        style: TextStyle(
-          fontSize: 40.0,
-          fontWeight: FontWeight.bold,
-          color: Colors.cyan,
-        ),
-      )
-    );
-  }
-} 
