de54bf6bd9bf391d437e04e9e1bbba6e37c7b547
chiapin <chiapin@hellotalk.com>
2019-08-27 22:05:54 +0800

    add box_decoration

--- lib/demo/basic_demo.dart
+++ lib/demo/basic_demo.dart
@@ -3,8 +3,91 @@ import 'package:flutter/material.dart';
 class BasicDemo extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    
+    return ContainerDemo();
+  }
+}
 
+class ContainerDemo extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      // color: Colors.grey[100],
+      decoration: BoxDecoration(
+        image: DecorationImage(
+          image: NetworkImage('https://resources.ninghao.org/images/candy-shop.jpg'),
+          alignment: Alignment.topCenter,
+          // repeat: ImageRepeat.repeatY,
+          fit: BoxFit.cover,
+          colorFilter: ColorFilter.mode(
+            Colors.indigoAccent[100].withOpacity(0.5),
+            BlendMode.hardLight,
+          ),
+        )
+      ),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Container(
+            child: Icon(Icons.pool, size: 32.0, color: Colors.white),
+            // color: Color.fromRGBO(3, 54, 255, 1.0),
+            padding: EdgeInsets.all(16.0),
+            margin: EdgeInsets.all(8.0),
+            width: 90.0,
+            height: 90.0,
+            decoration: BoxDecoration(
+              color: Color.fromRGBO(3, 54, 255, 1.0),
+              // border: Border(
+              //   top: BorderSide(
+              //     color: Colors.indigoAccent[100],
+              //     width: 3.0,
+              //     style: BorderStyle.solid,
+              //   ),
+              //   bottom: BorderSide(
+              //     color: Colors.indigoAccent[100],
+              //     width: 3.0,
+              //     style: BorderStyle.solid,
+              //   )
+              // ),
+              border: Border.all(
+                color: Colors.red,
+                width: 5.0,
+                style: BorderStyle.solid,
+              ),
+              // borderRadius: BorderRadius.circular(16.0),
+              // borderRadius: BorderRadius.only(
+              //   topLeft: Radius.circular(45.0), 
+              //   topRight: Radius.circular(45.0),
+              // )
+              boxShadow: [
+                BoxShadow(
+                  offset: Offset(6.0, 7.0),
+                  color: Color.fromRGBO(16, 20, 188, 1.0),
+                  blurRadius: 25.0, // 模糊程度
+                  spreadRadius: -9.0, // 扩散程度
+                )
+              ],
+              shape: BoxShape.circle, // 形状
+              gradient: LinearGradient(
+                colors: [
+                  // Color.fromRGBO(7, 102, 255, 1.0),
+                  // Color.fromRGBO(3, 28, 128, 1.0),
+                  Colors.red,
+                  Colors.blue,
+                ],
+                begin: Alignment.topCenter,
+                end: Alignment.bottomCenter,
+              )
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
+
+class RichTextDemo extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
     return RichText(
       text: TextSpan(
         text: 'nihao',
@@ -34,7 +117,7 @@ class TextDemo extends StatelessWidget {
     );
 
   final String _author = '李白';
-  final String _title = '将敬酒';
+  final String _title = '将进酒';
 
   @override
   Widget build(BuildContext context) {
