e3b7200face14faa4f510f575e73d2887f19bbf9
chiapin <chiapin@hellotalk.com>
2019-09-07 22:31:26 +0800

    add SizedBox

---
+++ lib/demo/layout_demo.dart
@@ -0,0 +1,73 @@
+import 'package:flutter/material.dart';
+
+class LayoutDemo extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // return IconBadge(Icons.pool);
+    // return Column(
+    //   children: <Widget>[
+    //     IconBadge(Icons.pool, size: 64.0,)
+    //   ],
+    // );
+    return Container(
+      child: Column(
+        // crossAxisAlignment: CrossAxisAlignment.stretch, //交叉轴
+        // mainAxisAlignment: MainAxisAlignment.spaceEvenly, // 主轴
+        // children: <Widget>[
+        //   IconBadge(Icons.pool,),
+        //   IconBadge(Icons.beach_access, size: 64.0,),
+        //   IconBadge(Icons.airplanemode_active),
+        // ],
+
+        // 设置SizedBox大小
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          SizedBox(
+            child: Container(
+              width: 200.0,
+              height: 300.0,
+              decoration: BoxDecoration(
+                color: Color.fromRGBO(3, 54, 255, 1.0),
+                borderRadius: BorderRadius.circular(8.0),
+              ),
+              child: Icon(Icons.ac_unit, color: Colors.white, size: 32.0,),
+            ),
+          ),
+          SizedBox(
+            height: 32.0,
+          ),
+          SizedBox(
+            child: Container(
+              width: 200.0,
+              height: 300.0,
+              decoration: BoxDecoration(
+                color: Color.fromRGBO(3, 54, 255, 1.0),
+                borderRadius: BorderRadius.circular(8.0),
+              ),
+              child: Icon(Icons.brightness_2, color: Colors.white, size: 32.0,),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class IconBadge extends StatelessWidget {
+  final IconData icon;
+  final double size;
+  
+  IconBadge(this.icon, {
+    this.size = 32.0
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Icon(icon, size: size, color: Colors.white,),
+      width: size + 60,
+      height: size + 60,
+      color: Color.fromRGBO(3, 54, 255, 1.0),
+    );
+  }
+}
