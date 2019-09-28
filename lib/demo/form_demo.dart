dc69d558b26a30491a794f50454f0a13cffc6c79
chiapin <chiapin@hellotalk.com>
2019-09-28 16:18:24 +0800

    init TextField

--- lib/demo/form_demo.dart
+++ lib/demo/form_demo.dart
@@ -11,7 +11,37 @@ class FormDemo extends StatelessWidget {
         data: Theme.of(context).copyWith(
           primaryColor: Colors.black,
         ),
-        child: ThemeDemo(),
+        // child: ThemeDemo(),
+        child: Container(
+          padding: EdgeInsets.all(8.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              TextFieldDemo(),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class TextFieldDemo extends StatefulWidget {
+  @override
+  _TextFieldDemoState createState() => _TextFieldDemoState();
+}
+
+class _TextFieldDemoState extends State<TextFieldDemo> {
+  @override
+  Widget build(BuildContext context) {
+    return TextField(
+      decoration: InputDecoration(
+        icon: Icon(Icons.subject),
+        labelText: 'Title',
+        hintText: 'Enter the post title', // 默认背景文字
+        // border: InputBorder.none,  // 边框
+        // border: OutlineInputBorder(),
+        filled: true,
       ),
     );
   }
